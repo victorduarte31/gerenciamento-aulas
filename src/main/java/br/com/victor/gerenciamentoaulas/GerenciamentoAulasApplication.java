e377ac90f1f409e0549ed72a4248497ceb5b4a2a
Victor Duarte <victormendonca@sisconsultoria.com.br>
2024-02-20 15:55:04 -0300

    primeiro commit curso de gerenciamento de aulas e professores.

---
+++ src/main/java/br/com/victor/gerenciamentoaulas/GerenciamentoAulasApplication.java
@@ -0,0 +1,13 @@
+package br.com.victor.gerenciamentoaulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoAulasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoAulasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/victor/gerenciamentoaulas/GerenciamentoAulasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victor.gerenciamentoaulas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoAulasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
